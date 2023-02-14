d1819822b5e88ad581bff386dcfe332d5936e79a
lialaurindo <liamaralaurindo@gmail.com>
2023-02-13 21:19:38 -0300

    resolução exercicios lista 01 - beecrowd

--- aula_01/src/exercicios_extras/AreaCirculo.java
+++ aula_01/src/exercicios_extras/AreaCirculo.java
@@ -1,10 +1,41 @@
 package exercicios_extras;
 
+import java.util.Scanner;
+
 public class AreaCirculo {
 
 	public static void main(String[] args) {
 		//BEECROWD 1002
+		
+		/*
+		 *  Cacular area de uma circunferência dado número do raio.
+		 *  Area = π . raio2. 
+		 *  Considera π = 3.14159
+		 *  Efetua cálculo de raio ao quadrado e retorna valor da area.
+		 *  
+		 */
+		Scanner leia = new Scanner(System.in);
 
+		double raio = leia.nextDouble();
+		double n = 3.14159;
+		double area = n*(Math.pow(raio, 2));
+		
+		System.out.printf("A=%.4f",(area),"\n");
+		
+		/*
+		 * Esperado: 
+		 * 2,00
+		 * A=12,5664
+		 * 
+		 *100,64
+		 *A=31819,3103
+		 * 
+		 * 150,00
+		 * A=70685,7750
+		 */
+
+		
+		
 	}
 
 }
