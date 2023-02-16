86dfd9f50cfc867f8e9ae11621b5b78dcff299bf
lialaurindo <liamaralaurindo@gmail.com>
2023-02-15 21:28:58 -0300

    resolução exercicios para entrega ArrayList/Set

--- aula_01/src/aula_05/exercicios_entrega/ArrayList_AdicionaCores.java
+++ aula_01/src/aula_05/exercicios_entrega/ArrayList_AdicionaCores.java
@@ -1,8 +1,76 @@
 package aula_05.exercicios_entrega;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class ArrayList_AdicionaCores {
 
 	public static void main(String[] args) {
+		
+		
+		/* 
+		 Exercício: organização de lista com Strings 
+
+		 [x] Solicita entrada de 5 nomes de cores 
+		 [x] Inclui as cores de entrada na Arraylist individualmente
+		 [x] Exibição1:  todas as cores adicionadas. 
+		 [x] Exibição2: todas as cores adicionadas ordenadas em ordem crescente (A-Z)
+
+		 */
+		
+		ArrayList<String> cores = new ArrayList<String>();
+
+		Scanner leia = new Scanner(System.in);
+
+		String cor;
+
+		System.out.println("Você deve digitar o nome de 5 cores: \n");
+
+		for (int contador = 1; contador <= 5; contador++) {
+			System.out.println("Digite a cor nº " + contador);
+			leia.skip("\\R?");
+			cor = leia.nextLine();
+			cores.add(cor);
+
+		}
+
+		System.out.println("\nLista de todas as cores digitadas: ");
+		cores.forEach(System.out::println);
+
+		System.out.println("\nLista ordenada (A-Z): ");
+		Collections.sort(cores);
+		cores.forEach(System.out::println);
+		
+		/*
+		 * Esperado:
+		 * Você deve digitar o nome de 5 cores: 
+		 * Digite a cor nº 1
+		 * Azul
+		 * Digite a cor nº 2
+		 * Verde
+		 * Digite a cor nº 3
+		 * Amarelo
+		 * Digite a cor nº 4
+		 * Branco
+		 * Digite a cor nº 5
+		 * Laranja
+		 * 
+		 * Lista de todas as cores digitadas: 
+		 * Azul
+		 * Verde
+		 * Amarelo
+		 * Branco
+		 * Laranja
+		 * 
+		 * Lista ordenada (A-Z): 
+		 * Amarelo
+		 * Azul
+		 * Branco
+		 * Laranja
+		 * Verde
+		 */
+
 
 	}
 }
--- aula_01/src/aula_05/exercicios_entrega/Set_Wrapper_AdicionaNumeros.java
+++ aula_01/src/aula_05/exercicios_entrega/Set_Wrapper_AdicionaNumeros.java
@@ -1,10 +1,74 @@
 package aula_05.exercicios_entrega;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Set_Wrapper_AdicionaNumeros {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		/* 
+		 	 Exercício: adiciona Inteiros na Collection Set
 
+		 [x] Solicita entrada de 10 números Inteiros;
+		 [x] Exibe todos os elementos da Collection Set, usando Classe Iterator. 
+		 
+		 */
+
+		Scanner leia = new Scanner(System.in);
+
+		Set<Integer> numeros = new HashSet<Integer>();
+		int valor;
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Digite o " + i + ("º número"));
+			valor = leia.nextInt();
+			numeros.add(valor);
+		}
+
+		Iterator<Integer> iNumeros = numeros.iterator();
+		System.out.println("\nLista de dados: \n");
+		
+		while (iNumeros.hasNext())
+			System.out.println(iNumeros.next());
+		
+		/*
+		 * Digite o 1º número 
+		 * 2
+		 * Digite o 2º número 
+		 * 5
+		 * Digite o 3º número 
+		 * 1 
+		 * Digite o 4º número 
+		 * 3
+		 * Digite o 5º número 
+		 * 4
+		 * Digite o 6º número 
+		 * 9
+		 * Digite o 7º número 
+		 * 7
+		 * Digite o 8º número 
+		 * 8
+		 * Digite o 9º número 
+		 * 10
+		 * Digite o 10º número 
+		 * 6
+		 * 
+		 * Lista de dados: 
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 * 6
+		 * 7
+		 * 8
+		 * 9
+		 * 10
+		 */
+		
 	}
-
 }
