136bbe7d939a0e41b57a4eb601911d993016af73
lialaurindo <liamaralaurindo@gmail.com>
2023-02-16 21:09:46 -0300

    adiciona exercicios realizados em aula

---
+++ aula_01/src/aula_06/Calculos.java
@@ -0,0 +1,65 @@
+package aula_06;
+
+public class Calculos {
+
+	public double soma(double numero1, double numero2) {
+		return numero1 + numero2;
+	}
+
+	public double subtracao(double numero1, double numero2) {
+		return numero1 - numero2;
+	}
+
+	public double multiplicacao(double numero1, double numero2) {
+		return numero1 * numero2;
+	}
+
+	public double divisao(double numero1, double numero2) {
+		return numero1 / numero2;
+	}
+	
+		/*
+		Esperado: 
+	
+		1 - Soma
+		2 - Subtração
+		3 - Multiplicação
+		4 - Divisão
+
+		Digite o número da Operação: 1
+		Digite o primeiro número: 5
+		Digite o segundo número: 5
+		Soma: 10.0
+
+		1 - Soma
+		2 - Subtração
+		3 - Multiplicação
+		4 - Divisão
+
+		Digite o número da Operação: 2
+		Digite o primeiro número: 5
+		Digite o segundo número: 5
+		Subtração: 0.0
+		
+		1 - Soma
+		2 - Subtração
+		3 - Multiplicação
+		4 - Divisão
+
+		Digite o número da Operação: 3
+		Digite o primeiro número: 5
+		Digite o segundo número: 5
+		Multiplicação: 25.0
+		
+		1 - Soma
+		2 - Subtração
+		3 - Multiplicação
+		4 - Divisão
+
+		Digite o número da Operação: 4
+		Digite o primeiro número: 5
+		Digite o segundo número: 5
+		Divisão: 1.0
+
+	 	*/
+}
