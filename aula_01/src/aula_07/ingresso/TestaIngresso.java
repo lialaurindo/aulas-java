3c76721c30e72a44b5f8692328a6da38dae0a9b1
lialaurindo <liamaralaurindo@gmail.com>
2023-02-23 14:43:04 -0300

    instancia objetos das subclasses Vip e MeiaEntrada

--- aula_01/src/aula_07/ingresso/TestaIngresso.java
+++ aula_01/src/aula_07/ingresso/TestaIngresso.java
@@ -9,16 +9,33 @@ public class TestaIngresso {
 		 * Utilizar o Método visualizar() para exibir dados dos 2 Objetos Instanciados.
 		 */		
 		
-		Ingresso ingressoCliente1 = new Ingresso ("ESPAÇO ITAÚ", "14-02-2019", "Minha Fama de Mau", 
-				"Meia-Entrada", "H 5", "Sala 05", "19h00", 80590, 16.00);
+		Ingresso ingressoCliente1 = new Ingresso ("ESPAÇO ITAÚ", "24/02/2023", "Minha Fama de Mau", 
+				"Meia-Entrada", "H 5", "Sala 05", "19h00", 80590, 32.00);
 		
-		Ingresso ingressoCliente2 = new Ingresso ("ESPAÇO ITAÚ", "14-02-2019", "Minha Fama de Mau", 
-				"Meia-Entrada", "H 6", "Sala 05", "19h00", 80591, 16.00);
+		Ingresso ingressoCliente2 = new Ingresso ("ESPAÇO ITAÚ", "24/02/2023", "Minha Fama de Mau", 
+				"Meia-Entrada", "H 6", "Sala 05", "19h00", 80591, 32.00);
+		
+		
+		Vip vipCliente3 = new Vip ("ESPAÇO ITAÚ", "24/02/2023", "Minha Fama de Mau", 
+				"Meia-Entrada", "H 7", "Sala 05", "19h00", 80580, 00.00, "24/02/2023");
+		
+		Vip vipCliente4 = new Vip ("ESPAÇO ITAÚ", "24/02/2023", "Minha Fama de Mau", 
+				"Meia-Entrada", "H 8", "Sala 05", "19h00", 80581, 00.00, "24/02/2023");
+		
+		
+		MeiaEntrada meiaEntradaCliente5 = new MeiaEntrada ("ESPAÇO ITAÚ", "24/02/2023", "Minha Fama de Mau", 
+				"Meia-Entrada", "I 5", "Sala 05", "19h00", 80570, 16.00, 963696);
+		
+		MeiaEntrada meiaEntradaCliente6 = new MeiaEntrada ("ESPAÇO ITAÚ", "24/02/2023", "Minha Fama de Mau", 
+				"Meia-Entrada", "I 6", "Sala 05", "19h00", 80570, 16.00, 963697);
+		
 		
 		ingressoCliente1.visualizar();
 		ingressoCliente2.visualizar();
-		
-
+		vipCliente3.visualizar();
+		vipCliente4.visualizar();
+		meiaEntradaCliente5.visualizar();
+		meiaEntradaCliente6.visualizar();
 	}
 
 }
