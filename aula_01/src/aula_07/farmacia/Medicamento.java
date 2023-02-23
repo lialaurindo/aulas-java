0dded91b7c82b5dc7004c36b6a091e3237f31af1
lialaurindo <liamaralaurindo@gmail.com>
2023-02-23 09:59:25 -0300

    cria subclasses de Farmacia > Medicamento e Perfumaria

---
+++ aula_01/src/aula_07/farmacia/Medicamento.java
@@ -0,0 +1,28 @@
+package aula_07.farmacia;
+
+public class Medicamento extends Farmacia {
+	
+	private String principioAtivo;
+
+	public Medicamento(long id, String nome, String nomeComercial, String fabricante, String foto, float preco,
+			int tipo, String principioAtivo) {
+		super(id, nome, nomeComercial, fabricante, foto, preco, tipo);
+		this.principioAtivo = principioAtivo;
+	}
+
+	public String getPrincipioAtivo() {
+		return principioAtivo;
+	}
+
+	public void setPrincipioAtivo(String principioAtivo) {
+		this.principioAtivo = principioAtivo;
+	}
+
+	@Override
+	//regra para reajuster preço dos remedios
+	public void reajuste(float percentual) {
+		this.setPreco((this.getPreco() * percentual) + 0.1f);
+		
+	}
+
+}
---
+++ aula_01/src/aula_07/farmacia/Perfumaria.java
@@ -0,0 +1,27 @@
+package aula_07.farmacia;
+
+public class Perfumaria extends Farmacia {
+	
+	private String fragrancia;
+
+	public Perfumaria(long id, String nome, String nomeComercial, String fabricante, String foto, float preco, int tipo,
+			String fragrancia) {
+		super(id, nome, nomeComercial, fabricante, foto, preco, tipo);
+		this.fragrancia = fragrancia;
+	}
+
+	public String getFragrancia() {
+		return fragrancia;
+	}
+
+	public void setFragrancia(String fragrancia) {
+		this.fragrancia = fragrancia;
+	}
+
+	@Override
+	public void reajuste(float percentual) {
+		this.setPreco((this.getPreco() * percentual) + 0.1f);
+		
+	}
+
+}
