a9a7784f41e05c3766a39a6b6c88e7900c0773ef
lialaurindo <liamaralaurindo@gmail.com>
2023-02-23 14:40:04 -0300

    cria subclasses com relacionamento de herança

--- aula_01/src/aula_07/ingresso/Vip.java
+++ aula_01/src/aula_07/ingresso/Vip.java
@@ -4,21 +4,28 @@ import java.sql.Date;
 
 public class Vip extends Ingresso{
 	
-	private Date dataValidade;
+	private String dataValidade;
 	
 	public Vip(String nomeEstabelecimento, String dataIngresso, String tituloFilme, String tipoIngresso,
-			String assentoSala, String salaFilme, String horarioFilme, Integer codigoIngresso, Double valorIngresso) {
+			String assentoSala, String salaFilme, String horarioFilme, Integer codigoIngresso, Double valorIngresso,
+			String dataValidade) {
 		super(nomeEstabelecimento, dataIngresso, tituloFilme, tipoIngresso, assentoSala, salaFilme, horarioFilme,
 				codigoIngresso, valorIngresso);
-	}
-
-	public Date getDataValidade() {
-		return dataValidade;
-	}
-
-	public void setDataValidade(Date dataValidade) {
 		this.dataValidade = dataValidade;
 	}
 
+	public String getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(String dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+	
+	public void visualizar() {
+		
+		super.visualizar();
+		System.out.println("Número de rodas: " + this.dataValidade);
+	}
 	
 }
